1c027b9f4b2c9f3e788c64dfca9903b66530a7c7
D7703_02 <d7703_02@D7703-02ui-iMac.local>
2018-04-04 15:08:24 +0900

    123

--- Alienimage Timer/ViewController.swift
+++ Alienimage Timer/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var counter = 1
+    var myTimer = Timer()
+    var direction = 1
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var imageCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageCounter.text = String(counter)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btStart(_ sender: UIButton) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
     }
+    
+    @IBAction func btStop(_ sender: UIButton) {
+        myTimer.invalidate()
+    }
+    
 
-
+    @objc func doAnimation(){
+      //  if counter == 5 {
+      //      counter = 1
+      // } else {
+      //      counter = counter + 1
+            
+      //  }
+        
+        
+        if counter == 5{
+            direction = 0
+        }
+        else if counter == 1{
+            direction = 1
+        }
+        if direction == 1{
+            counter += 1
+        }
+        else if direction == 0{
+            counter = counter - 1
+        }
+        
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        imageCounter.text = String(counter)
+    }
 }
 
